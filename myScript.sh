0bafc2f94480d6dcdc14dd36804f399447d15cf3
hwndk <143599558+hwndk@users.noreply.github.com>
2023-09-20 19:11:51 +0900

    Add files via upload

---
+++ myScript.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd ./files
+
+mv a* ../a
+mv A* ../a
+mv b* ../b
+mv B* ../b
+mv c* ../c
+mv C* ../c
+mv d* ../d
+mv D* ../d
+mv e* ../e
+mv E* ../e
+mv f* ../f
+mv F* ../f
+mv g* ../g
+mv G* ../g
+mv h* ../H
+mv H* ../H
+mv i* ../i
+mv I* ../i
+mv j* ../j
+mv J* ../j
+mv k* ../k
+mv K* ../k
